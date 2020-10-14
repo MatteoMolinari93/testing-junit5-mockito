67600205833857b601d83d091cca30914b35138d
Molinari <matteomolinari93@gmail.com>
2020-10-14 16:10:08 +0200

    Verify interactions with mocks

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.atLeastOnce;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,8 +22,23 @@ public class SpecialitySDJpaServiceTest {
 	SpecialitySDJpaService service;
 	
 	@Test
-	void delete() {
+	void deleteById() {
 		service.deleteById(1l);
+		
+		verify(specialtyRepository).deleteById(1l);
+	}
+	
+	@Test
+	void deleteByIdAtLeastOnce() {
+		service.deleteById(1l);
+		service.deleteById(1l);
+		
+		verify(specialtyRepository, atLeastOnce()).deleteById(1l);
+	}
+	
+	@Test
+	void delete() {
+		service.delete(new Speciality());
 	}
 
 }
